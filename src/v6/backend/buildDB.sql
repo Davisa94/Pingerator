3436fce169fe52e33607ffa2069023a5e881b033
Davisa94 <Austin.i.davis94@gmail.com>
2021-07-29 22:12:22 -0700

    Completed speed table

--- src/v6/backend/buildDB.sql
+++ src/v6/backend/buildDB.sql
@@ -5,15 +5,25 @@ use pingerator;
     -- we need speed value
     -- we need ip address
 CREATE table speed (
-    datetime_tested TIMESTAMP NOT NULL,
+    test_time_id INT NOT NULL,
     downspeed_value DOUBLE NULL,
     upspeed_value DOUBLE NULL,
-
-)
+    CONSTRAINT test_time_id
+        FOREIGN KEY(test_time_id)
+        REFERENCES testTime (test_time_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT);
 
 CREATE table pings (
-    
-)
+    test_time_id INT NOT NULL,
+    pingresponse_value FLOAT NULL,
+    upspeed_value DOUBLE NULL,
+    CONSTRAINT test_time_id
+        FOREIGN KEY(test_time_id)
+        REFERENCES testTime (test_time_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+);
 -- we need a table for the pings
     -- test_time_id fk > testtime table
     -- we need ping response time or NULL if none
@@ -26,10 +36,19 @@ CREATE table pings (
     -- test_time_id fk > testtime table
     -- domain name
     -- 
-
+CREATE table ipLookup(
+    
+);
 -- incidint table
 
 -- testtime table 
     -- id primary
     -- datetime
     -- 
+
+CREATE table testTime(
+    test_time_id SERIAL NOT NULL,
+    datetime_tested TIMESTAMP NOT NULL,
+    PRIMARY KEY(test_time_id),
+    UNIQUE(test_time_id));
+)
