f62e88044f89cc274e036a7b8e211ba3a14a531f
Davisa94 <Austin.i.davis94@gmail.com>
2021-07-29 21:15:50 -0700

    added a file to house the sql to build the DB

---
+++ src/v6/backend/buildDB.sql
@@ -0,0 +1,15 @@
+-- We need a table for the speeds
+    -- we need timestamp
+    -- we need speed value
+    -- we need ip address
+
+
+-- we need a table for the pings
+    -- we need timestamp
+    -- we need ping response time or NULL if none
+    -- we need 
+
+-- ip lookup:
+    -- id
+    -- timestamp
+    -- 
