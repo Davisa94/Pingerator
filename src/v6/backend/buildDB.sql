5676ddeca1b0e6a9521d1b6148f377209970ad88
Davisa94 <Austin.i.davis94@gmail.com>
2021-07-29 22:25:39 -0700

    Completed preliminary sql

--- src/v6/backend/buildDB.sql
+++ src/v6/backend/buildDB.sql
@@ -18,12 +18,15 @@ CREATE table pings (
     test_time_id INT NOT NULL,
     pingresponse_value FLOAT NULL,
     upspeed_value DOUBLE NULL,
+    ipv4_id INT NOT NULL,
     CONSTRAINT test_time_id
         FOREIGN KEY(test_time_id)
-        REFERENCES testTime (test_time_id)
+        REFERENCES testTime (id)
         ON DELETE RESTRICT
-        ON UPDATE RESTRICT
-);
+        ON UPDATE RESTRICT,
+    CONSTRAINT ipv4_id
+        FOREIGN KEY(ipv4_id)
+        REFERENCES ipLookup (id));
 -- we need a table for the pings
     -- test_time_id fk > testtime table
     -- we need ping response time or NULL if none
@@ -37,8 +40,12 @@ CREATE table pings (
     -- domain name
     -- 
 CREATE table ipLookup(
-    
-);
+    id SERIAL NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    ipv4 VARCHAR(15),
+    domain_name VARCHAR(80),
+    PRIMARY KEY(id),
+    UNIQUE(id));
 -- incidint table
 
 -- testtime table 
@@ -47,8 +54,8 @@ CREATE table ipLookup(
     -- 
 
 CREATE table testTime(
-    test_time_id SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     datetime_tested TIMESTAMP NOT NULL,
-    PRIMARY KEY(test_time_id),
+    PRIMARY KEY(id),
     UNIQUE(test_time_id));
 )
