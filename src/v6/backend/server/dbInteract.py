215aa47e2d27b8764ca1db612ef5cacd47f4e2c6
Davisa94 <Austin.i.davis94@gmail.com>
2021-07-30 23:38:45 -0700

    created a DB interaction module for querying the DB

--- src/v6/backend/server/dbInteract.py
+++ src/v6/backend/server/dbInteract.py
@@ -1,36 +1,11 @@
-import MySQLdb as sql
-
-
 import pymysql
- 
-class MyDb:
 
-    def __init__(self):
-        self.schema="pingerator"
-        self.host=""
-        self.user=""
-        self.password=""
 
-    def dbDebugDisplay(self, dbObject, statement):
-        cursor = self.dbInteract(statement, dbObject)
-        print(self.dbData2Array(cursor))
+class DBInteract:
+    def __init__(self, dbObject=""):
+        self.DBO = dbObject
 
-        
-    def dbInteract(self, statment:str, dbConnection)->"Returns the cursor":
-        #create cursor
-        cursor = dbConnection.cursor()
-        
-        #execute the query
-        cursor.execute(statment)
-        return cursor
-
-    def dbData2Array(self, cursor):
-        tableData = cursor.fetchall()
-        return tableData
-
-    def dbConnect(self) ->"dont forget to close the connection":
-        mysql_connection = pymysql.connect(self.host, self.user, self.password, self.schema)
-        mysql_connection.cursor()
-        return mysql_connection
-    
-
+    def getSpeeds(self):
+        sql = ("SELECT * FROM speeds")
+        cursor.execute(sql)
+        return dbObject
